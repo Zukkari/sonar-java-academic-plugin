7848639bd6cfd8079f63af18949d08c2feb17a35
Zukkari <stanislav.myshkovski@gmail.com>
2019-11-24 12:35:48 +0200

    #8: add case 1 where number of methods is low

--- src/main/scala/io/github/zukkari/checks/LazyClass.scala
+++ src/main/scala/io/github/zukkari/checks/LazyClass.scala
@@ -1,10 +1,15 @@
 package io.github.zukkari.checks
 import org.sonar.check.Rule
 import org.sonar.plugins.java.api.JavaFileScannerContext
+import org.sonar.plugins.java.api.tree.{ClassTree, MethodTree}
+
+import scala.jdk.CollectionConverters._
 
 @Rule(key = "LazyClassRule")
 class LazyClass extends JavaRule {
   private var context: JavaFileScannerContext = _
+
+  private val minNumberOfMethods = 0
 
   override def scannerContext: JavaFileScannerContext = context
 
@@ -12,5 +17,13 @@ class LazyClass extends JavaRule {
     this.context = javaFileScannerContext
 
     scan(context.getTree)
+  }
+
+  override def visitClass(tree: ClassTree): Unit = {
+    // Case 1: detect classes with low number of methods
+    val methods = tree.members.asScala.filter(_.isInstanceOf[MethodTree])
+    report(s"Lazy class: number of methods is lower or equal to: ${minNumberOfMethods}", tree, methods.size <= minNumberOfMethods)
+
+    super.visitClass(tree)
   }
 }
--- src/test/resources/files/lazy_class/LazyClassBasic.java
+++ src/test/resources/files/lazy_class/LazyClassBasic.java
@@ -1,3 +1,11 @@
-public class LazyClassBasic {
+public class LazyClassBasic { // Noncompliant: {{Lazy class: number of methods is lower or equal to: 0}}
 
+}
+
+class LazyClassWithMembers {
+    public void m1() {
+    }
+
+    public void m2() {
+    }
 }
