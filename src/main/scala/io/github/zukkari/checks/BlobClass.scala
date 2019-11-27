2cc3c137f607480dffebee1701cd4c636f2a4040
Zukkari <stanislav.myshkovski@gmail.com>
2019-11-27 20:57:47 +0200

    #27: start with check implementation

--- src/main/scala/io/github/zukkari/checks/BlobClass.scala
+++ src/main/scala/io/github/zukkari/checks/BlobClass.scala
@@ -1,10 +1,20 @@
 package io.github.zukkari.checks
+
 import org.sonar.check.Rule
 import org.sonar.plugins.java.api.JavaFileScannerContext
+import org.sonar.plugins.java.api.tree.{ClassTree, MethodTree, VariableTree}
+import org.sonar.plugins.java.api.tree.Tree.Kind
+
+import scala.jdk.CollectionConverters._
 
 @Rule(key = "BlobClass")
 class BlobClass extends JavaRule {
   private var context: JavaFileScannerContext = _
+
+  private val numberOfVariables = 13
+  private val numberOfMethods = 22
+
+  private val lackOfCohesion = 40
 
   override def scanFile(javaFileScannerContext: JavaFileScannerContext): Unit = {
     this.context = javaFileScannerContext
@@ -13,4 +23,33 @@ class BlobClass extends JavaRule {
   }
 
   override def scannerContext: JavaFileScannerContext = context
+
+  override def visitClass(tree: ClassTree): Unit = {
+    // Find variables of the class
+    val variables = tree.members.asScala
+      .filter(_.is(Kind.VARIABLE))
+      .map(_.asInstanceOf[VariableTree])
+
+    if (variables.size < numberOfVariables) {
+      // Number of variables lower than threshold
+      // so no point in going further
+      return
+    }
+
+    // Find methods of the class
+    val methods = tree.members.asScala
+      .filter(_.is(Kind.METHOD))
+      .map(_.asInstanceOf[MethodTree])
+
+    if (methods.size < numberOfMethods) {
+      // Number of methods is lower than threshold
+      // no point in going further
+      return
+    }
+
+    // Calculate cohesion between variables
+
+
+    super.visitClass(tree)
+  }
 }
