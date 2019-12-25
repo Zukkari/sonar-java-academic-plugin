7cefc1967df8977e8515b2b948fe4f39c229942e
Zukkari <stanislav.myshkovski@gmail.com>
2019-12-25 15:26:36 +0200

    #47: implement feature envy

--- src/test/scala/io/github/zukkari/checks/FeatureEnvySpec.scala
+++ src/test/scala/io/github/zukkari/checks/FeatureEnvySpec.scala
@@ -5,4 +5,9 @@ import io.github.zukkari.RuleSpec
 class FeatureEnvySpec extends RuleSpec {
   override def dir: String = "feature_envy"
 
+  private val check = new FeatureEnvy
+
+  it should "detect feature envy" in {
+    verifyRule(check, "FeatureEnvy")
+  }
 }
